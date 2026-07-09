5c14b1b5292b2451fa0c032d790b900227853cfb
NamHa2001 <yu2401010517@yersin.edu.vn>
2026-07-09 20:20:17 +0700

    chuyển cấu hình dev sang windows auth và api localhost

--- frontend/health-plus/src/environments/environment.ts
+++ frontend/health-plus/src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  // apiUrl: 'http://localhost:5199/api',
-  apiUrl: 'https://hddtlp6f-5199.asse.devtunnels.ms/api',
+  apiUrl: 'http://localhost:5199/api',
+  // apiUrl: 'https://hddtlp6f-5199.asse.devtunnels.ms/api',
 };
